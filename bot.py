257a3b342497fd903ec14e41b58dd355287f5bcd
DanilRozhin <danilrozhin2005@mail.ru>
2025-01-24 04:18:46 +0300

    Just a print

--- bot.py
+++ bot.py
@@ -11,35 +11,35 @@ from aiogram.types import *
 TG_TOKEN = os.getenv('TG_TOKEN')
 print(TG_TOKEN)
 
-bot = Bot(token=TG_TOKEN)
-dp = Dispatcher()
+# bot = Bot(token=TG_TOKEN)
+# dp = Dispatcher()
 
 
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer('Hello!')
+# @dp.message(CommandStart())
+# async def cmd_start(message: Message):
+#     await message.answer('Hello!')
 
-@dp.message()
-async def cmd_text(message: Message):
-    user_id = message.chat.id
-    try:
-        await message.answer(message.text)
-    except:
-        await message.answer('Sorry, I can\'t understand this message yet')
+# @dp.message()
+# async def cmd_text(message: Message):
+#     user_id = message.chat.id
+#     try:
+#         await message.answer(message.text)
+#     except:
+#         await message.answer('Sorry, I can\'t understand this message yet')
 
-async def main():
-    today_date = datetime.date.today()
+# async def main():
+#     today_date = datetime.date.today()
 
-    url = 'https://www.championat.com/stat/#' + str(today_date)
-    response = requests.get(url, headers={
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
-    })
-    bs = str(BeautifulSoup(response.text, 'lxml'))
-    if 'Реал Мадрид' in bs:
-        await bot.send_message(1232626150, 'Real Madrid maybe playing today')
-    # await bot.send_message(418223763, 'Плановое выполнение скрипта, сообщите автору об успешном выполнении программы')
-    await bot.send_message(1232626150, 'Завершен')
-    # await dp.start_polling(bot)
+#     url = 'https://www.championat.com/stat/#' + str(today_date)
+#     response = requests.get(url, headers={
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+#     })
+#     bs = str(BeautifulSoup(response.text, 'lxml'))
+#     if 'Реал Мадрид' in bs:
+#         await bot.send_message(1232626150, 'Real Madrid maybe playing today')
+#     # await bot.send_message(418223763, 'Плановое выполнение скрипта, сообщите автору об успешном выполнении программы')
+#     await bot.send_message(1232626150, 'Завершен')
+#     # await dp.start_polling(bot)
 
-if __name__ == '__main__':
-    asyncio.run(main())
+# if __name__ == '__main__':
+#     asyncio.run(main())
